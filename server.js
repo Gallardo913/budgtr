ae7285d1980756c458649c8941fdb65f05393ac5
Roger gallardo <rogergallardofs1@gmail.com>
2022-11-21 23:24:53 -0500

    final?

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 3000;
 const budget = require("./models/budget.js")
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.send('Whats poppin')
 })
@@ -13,21 +16,20 @@ app.get('/budget', (req,res) => {
     })
 })
 
+app.get('/budget/new', (req, res) => {
+    res.render('new.ejs')
+})
+
 app.get('/budget/:id', (req, res) => {
     res.render('show.ejs', {
         allBudget: budget[req.params.id]
     })
 })
 
-
-
-
-
-
-
-
-
-
+app.post('/budget', (req, res) => {
+    budget.push(req.body),
+    res.redirect('budget')
+})
 
 app.listen(PORT, () => {
     console.log(`Wait and listen...`)
